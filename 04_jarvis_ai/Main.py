dee8c4f94ad0adf970d72082bb4774a0c6b77e26
sagorluc <mdsagorluc@gmail.com>
2023-06-14 15:17:49 +0600

    more project

---
+++ 04_jarvis_ai/Main.py
@@ -0,0 +1,91 @@
+from jarvis import*
+
+def main():
+    #speak('jarvis is a ai module')
+    wishme()
+    while True:
+        take_query = takecommand().lower()
+
+        if 'wikipedia' in take_query:
+            speak('Searching wikipedia...')
+            take_query = take_query.replace('wikipedia', '')
+            result = wikipedia.summary(take_query, sentences=2)
+            speak('According to wikipedia')
+            print(result)
+            speak(result)
+            
+        elif 'open youtube' in take_query:
+            webbrowser.open('youtube.com')
+
+        elif 'open google' in take_query:
+            webbrowser.open('google.com')
+
+        elif 'open facebook' in take_query:
+            webbrowser.open('facebook.com')
+
+        elif 'open instagram' in take_query:
+            webbrowser.open('instagram.com')
+
+        elif 'open twitter' in take_query:
+            webbrowser.open('twitter.com')
+
+        elif 'open linkedin' in take_query:
+            webbrowser.open('linkedin.com')
+
+        elif 'open messenger' in take_query:
+            webbrowser.open('messenger.com')
+
+        elif 'open whatsapp' in take_query:
+            webbrowser.open('whatsapp.com')
+
+        elif 'open blogger' in take_query:
+            webbrowser.open('blogger.com')
+
+        elif 'play music' in take_query:
+            # local drive path
+
+            # music_drive = "D\\drive\\path"   
+            # result = os.listdir(music_drive)
+            # print(result)
+            # os.startfile(os.path.join(music_drive, result[0]))
+
+            # website link
+            webbrowser.open('jango.com')
+
+        elif 'the time' in take_query:
+            start_time = datetime.now().strftime('%H: %M: %S')
+            cur_date = date.today()
+            day_of_week = cur_date.weekday()
+            dayy = cur_date.strftime('%D')
+            month = cur_date.strftime('%B')
+            speak(f'sir, the time is {start_time}') 
+            speak(f'and today is {cur_date}')
+            speak(f'and week day {day_of_week}')
+            speak(f'and today is {dayy}')
+            speak(f'and the month is {month}')
+
+        elif 'open code' in take_query:
+            vs_code_path = '"C:\\Users\\User\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"'
+            os.startfile(vs_code_path)
+
+        elif 'open email' in take_query:
+            webbrowser.open('gmail.com')
+
+        elif 'email to sagar' in take_query:
+
+            try:
+                speak('what should i say!')
+                contant = takecommand()
+                to = 'mdsagorluc@gmail.com'
+                send_email(to, contant)
+                speak('Email has been send!')
+            except Exception as e:
+                print(e)
+                speak('sorry sagar i am not able to send this email')
+
+
+        
+
+
+if __name__ == '__main__':
+    main()
---
+++ 04_jarvis_ai/jarvis.py
@@ -0,0 +1,71 @@
+import pyttsx3 # this module for voices
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib
+from datetime import datetime, date
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices') # tow voice here male and female
+#print(voices[0].id)
+engine.setProperty('voice', voices[1].id) # set female voice
+#engine.setProperty('voice', voices[0].id) # set male voice
+#print(male_voice)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    houre = int(datetime.now().hour)
+    
+    if houre >=0 and houre < 12:
+        speak("good morning sagar")
+    elif houre >= 12 and houre < 18:
+        speak("good afternoon sagar")
+    elif houre >= 18 and houre < 20:
+        speak("good evening sagar")
+    else:
+        speak("good night sagar")
+
+    speak('hei!i am jarvis ai developer sagar ahmed here.how may i help you')
+
+def takecommand():
+    # it takes microphone speech and return the string output
+    
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language="en-in")
+        print(f'he/she said: {query}\n')
+
+    except Exception as ex:
+        #print(ex) # don't show the error 
+        print('Please say that agin!')
+        return 'None'
+    return query
+
+def send_email(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo() # build in function of smtplib
+    server.starttls() # build in function of smtplib
+    server.login('droptosagor@gmail.com', 'sagor102030')  # build in function of smtplib
+    server.sendmail('mdsagorluc@gmail.com', to, content)  # build in function of smtplib
+    server.close()
+
+       
+
+
+
+
+
+# if __name__ == '__main__':
+#     speak("have a good time")
+#     wishme()
